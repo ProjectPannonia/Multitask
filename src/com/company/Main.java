1583f38ccbac90414fb686d5f6727bc74f3b0241
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2020-08-07 19:28:14 +0200

    Runnable and Thread

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,42 @@
 package com.company;
 
+class TaskerThread extends Thread{
+    public void run(){
+        for (int i = 0; i < 5; i++){
+            System.out.println("Mellékszál: " + i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+class TaskerRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++){
+            System.out.println("Mellékszál: " + i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	TaskerThread t1 = new TaskerThread();
+	TaskerThread t2 = new TaskerThread();
+	t1.start();
+	t2.start();
+
+	Thread t3 = new Thread(new TaskerRunnable());
+	Thread t4 = new Thread(new TaskerRunnable());
+	t3.run();
+	t4.run();
     }
 }
